6324b70474583b5d8e25d02c053e0b78ae2ec5c4
Aleksandr Ivannikov <a.n.ivannikov@yandex.ru>
2022-03-25 18:39:32 +0300

    Updates

--- static/js/11.c9ed60de.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonp@p202io/react-app"]=this["webpackJsonp@p202io/react-app"]||[]).push([[11],{409:function(p,a){}}]);
-//# sourceMappingURL=11.c9ed60de.chunk.js.map
---
+++ static/js/11.e55f7aa9.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@p202io/react-app"]=this["webpackJsonp@p202io/react-app"]||[]).push([[11],{415:function(p,a){}}]);
+//# sourceMappingURL=11.e55f7aa9.chunk.js.map
